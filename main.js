8ce0fac276dc945f6df40da5eea767b2c5e72a90
thehanir95 <39161188+thehanir95@users.noreply.github.com>
2019-11-30 12:51:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+/* first slider */
+
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots:true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next"
+});
+
+
+/* second slider */
+
+$(".slider-two")
+.not(".slick-initialized")
+.slick({
+   
+    prevArrow:".site-slider-two .prev",
+    nextArrow:".site-slider-two .next",
+    slidesToShow:5,
+    slidesToScroll:1,
+    autoplaySpeed:3000
+});
